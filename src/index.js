27467d7986ca8d9f4bbd41d6dee3bea317ec29d1
Jakob Ström <jacobo.strm@gmail.com>
2022-03-12 11:23:40 +0100

    kek

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { Builder, By } from 'selenium-webdriver';
-
 let driver = new Builder().forBrowser('firefox').build();
 await driver.get('https://bokapass.nemoq.se/Booking/Booking/Index/varmland');
 await driver.findElement(By.css('input[value="Boka ny tid"]')).click();
@@ -11,3 +10,4 @@ await driver.findElement(By.css('input[value="Första lediga tid"]')).click();
 let dateElement = await driver.findElement(By.id('dateText'));
 console.log(await dateElement.getText());
 await driver.quit();
+//# sourceMappingURL=index.js.map
